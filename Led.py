977a6930136dd1b05aa6c6eb602a91ac6b3fa1cf
Luigi Vincent <luigipvincent@gmail.com>
2015-10-01 00:51:42 -0400

    add error cleanup

--- Led.py
+++ Led.py
@@ -10,9 +10,9 @@ import time
 led = 17 #This depends on where your LED & Resistor are plugged in, use the GPIO Header Quick Reference for guidance
 
 GPIO.setmode(GPIO.BCM)
-GIPO.setwarnings(False)
 GPIO.setup(led, GPIO.OUT)
 
 GPIO.output(led, GPIO.HIGH)
 time.sleep(15)
 GPIO.output(led, GPIO.LOW)
+GPIO.cleanup()
